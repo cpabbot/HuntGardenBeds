8473b008a44567fa6379c582c88cdfb9e3f54b4a
Cameron Abbot <cpabbot1@gmail.com>
2020-07-04 00:05:58 -0400

    basic products page

--- js/main.js
+++ js/main.js
@@ -12,11 +12,18 @@
         var element = elements[i];
         var positionFromTop = elements[i].getBoundingClientRect().top;
         
-        if (positionFromTop - windowHeight <= -150) {
+        if (positionFromTop - windowHeight <= -175) {
           element.classList.add('fade-in');
           element.classList.remove('hidden');
           console.log(i + ": " + positionFromTop + " - " + windowHeight);
         }
+      }
+      
+      if(window.scrollY > 0) {
+        document.querySelectorAll('[shrink-on-scroll]')[0].classList.remove('padded');
+      }
+      else {
+        document.querySelectorAll('[shrink-on-scroll]')[0].classList.add('padded');
       }
     }
   
